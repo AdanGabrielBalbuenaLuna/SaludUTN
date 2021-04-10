5ec84065574abd35b43deed98aa984d7b9fa5fea
Gabriel <agbalbuenaluna@gmail.com>
2021-04-09 23:09:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/saludutn/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.saludutn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Find the TextView
+        TextView saludFisicaTV = (TextView)findViewById(R.id.textView_salud_fisica);
+
+        saludFisicaTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create an explicit intent
+                Intent saludFisicaIntent = new Intent(MainActivity.this, SaludFisica.class);
+
+                //Use intent method
+                startActivity(saludFisicaIntent);
+            }
+        });
+
+        //Find the TextView
+        TextView saludMetalTV = (TextView)findViewById(R.id.textView_salud_mental);
+
+        saludMetalTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create an explicit intent
+                Intent saludMentalIntent = new Intent(MainActivity.this, SaludMental.class);
+
+                //Use intent method
+                startActivity(saludMentalIntent);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/saludutn/SaludFisica.java
@@ -0,0 +1,14 @@
+package com.example.saludutn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaludFisica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salud_fisica);
+    }
+}
