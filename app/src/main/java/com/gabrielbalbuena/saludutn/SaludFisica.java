657e476c0b1a9212ee7509f22e4270b632d5d145
Gabriel <agbalbuenaluna@gmail.com>
2021-04-21 14:52:01 -0500

    Icon from Developer, need changes Before make changes in use cases Datos Personales Activity

--- app/src/main/java/com/gabrielbalbuena/saludutn/SaludFisica.java
+++ app/src/main/java/com/gabrielbalbuena/saludutn/SaludFisica.java
@@ -2,7 +2,10 @@ package com.gabrielbalbuena.saludutn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SaludFisica extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class SaludFisica extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salud_fisica);
+
+        //Find the TextView
+        TextView datosPersonalesTV = (TextView)findViewById(R.id.textView_datos_personales);
+
+        datosPersonalesTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create an explicit intent
+                Intent datosPersonalesIntent = new Intent(SaludFisica.this, DatosPersonales.class);
+
+                //Use intent method
+                startActivity(datosPersonalesIntent);
+            }
+        });
     }
 }
