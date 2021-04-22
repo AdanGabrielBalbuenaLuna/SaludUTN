cfd6d58832f80b6eed1c23071aa5e802ed98557b
Gabriel <agbalbuenaluna@gmail.com>
2021-04-22 00:11:45 -0500

    USE CASE DATOS PERSONALES Completed working 75%

--- app/src/main/java/com/gabrielbalbuena/saludutn/DatosPersonales.java
+++ app/src/main/java/com/gabrielbalbuena/saludutn/DatosPersonales.java
@@ -163,11 +163,12 @@ public class DatosPersonales extends AppCompatActivity implements LoaderManager.
     public boolean onOptionsItemSelected(MenuItem item) {
         // User clicked on a menu option in the app bar overflow menu
         switch (item.getItemId()) {
+            /*
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
                 insertDatosPersonales();
                 //displayDatabaseInfo();
-                return true;
+                return true;*/
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
                 // Do nothing for now
