40184d1754c41e03b0f4bf6ac60748a3bd2d7bbc
Gabriel <agbalbuenaluna@gmail.com>
2021-04-23 15:53:45 -0500

    USE CASE DIARIO DE EMOCIONES Completed working 15%
    USE CASE DATOS PERSONALES Completed
    working 99%

--- app/src/main/java/com/gabrielbalbuena/saludutn/SaludMental.java
+++ app/src/main/java/com/gabrielbalbuena/saludutn/SaludMental.java
@@ -2,7 +2,10 @@ package com.gabrielbalbuena.saludutn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SaludMental extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class SaludMental extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salud_mental);
+
+        //Find the TextView
+        TextView diarioEmocionesTV = (TextView)findViewById(R.id.textView_diario_de_emociones);
+
+        diarioEmocionesTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create an explicit intent
+                Intent datosPersonalesIntent = new Intent(SaludMental.this, DiarioEmociones.class);
+
+                //Use intent method
+                startActivity(datosPersonalesIntent);
+            }
+        });
     }
 }
